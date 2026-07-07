113f0e737c8a6564b71bb0aebb436c0f90218222
Christopher Reaume <christopher.reaume@automattic.com>
2026-07-07 04:21:10 -0700

    fix: make sure _repl node is included with the rest of the graph on first paint

--- includes/rest/class-topologies-ci-node.php
+++ includes/rest/class-topologies-ci-node.php
@@ -108,6 +108,7 @@ class Topologies_CI_Node extends Service_CI_Node {
 				\esc_html( "failed to read topology file: $path" )
 			);
 		}
+		$tsl .= "\nmake_node Partition _repl\n";
 
 		$sources = Topology_Registry::describe()[ $name ] ?? [
 			'user'  => null,
