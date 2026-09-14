fc9521ed83461f9b32dbc896dd2c91ed0258e549
Christopher Reaume <christopher.reaume@automattic.com>
2026-09-14 00:40:22 -0700

    chore(release): 2.58.3

--- src/build-kit/index.mjs
+++ src/build-kit/index.mjs
@@ -40,7 +40,7 @@ import path from 'node:path';
  *
  * @type {string}
  */
-const SUBSTRATE_VERSION = '2.58.2';
+const SUBSTRATE_VERSION = '2.58.3';
 
 /**
  * Read the substrate version this kit stamps into every bundle.
